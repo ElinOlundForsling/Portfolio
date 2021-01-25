6181d6d6594ebcb48bf20f318783d5767c9fc871
Elin Ölund Forsling <33157767+ElinOlundForsling@users.noreply.github.com>
2021-01-25 18:05:32 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Ahoy from './views/Ahoy';
+import Landing from './views/Landing';
+import Footer from './views/Footer';
+import enquire from 'enquire-js';
 
-function App() {
+let isMobile = false;
+enquire.register('screen and (max-width:40em)', {
+  match: function () {
+    isMobile = true;
+    console.log('true');
+  },
+  unmatch: function () {
+    isMobile = false;
+    console.log('false');
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Landing />
+      <Ahoy isMobile={isMobile} />
+      <Ahoy isMobile={isMobile} />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
